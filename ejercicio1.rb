e1c4e4164ab17dcda8e36bcc4e9efe64d6a095ee
evelyn <astudillobarra.evelyn@gmail.com>
2018-06-28 21:43:12 -0400

    casino exercise

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -1,8 +1,40 @@
-class Table
-  def initialize
+      class Table
+        attr_reader :mesa, :dinero
+        def initialize(mesa, *dinero)
+          @mesa = mesa
+          @dinero = dinero.map(&:to_i)
+        end
 
+      def Table.build(file)
+        tables = []
+      File.readlines(file).each do |line|
+      data = line.split(', ').map(&:chomp)
+      mesa, dinero = data.shift, data
+      tables << Table.new(mesa, *dinero)
+      end
+      return tables
+      end
+
+    def Table.max_val(file)
+     tables =  build(file)
+     best_day = {}
+     tables.each do |table|
+     best_day[table.mesa] = [table.max, table.max_day]
+    end
+    p best_day
+    end
+
+    def Table.average(file)
+       #@dinero.inject(&:+) / @dinero.size.to_f
+    end
+
+     def max
+       dinero.max
+    end
+
+    def max_day
+      dinero.index(max) + 1
+      end
   end
-end
- file = File.open('casino.txt', 'r')
- p data = file.readlines
- file.close
+
+     Table.max_val('casino.txt')
